e39ad0693665b19cfe9a9175543e5c64593580f1
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-05-05 23:51:52 +0300

    исправил

--- scripts/index.js
+++ scripts/index.js
@@ -49,20 +49,16 @@ window.addEventListener('DOMContentLoaded', () => {
     //меню
     const toggleMenu = () => {
         const menu = document.querySelector('menu');
-        const handlerMenu = () => {
-            menu.classList.toggle('active-menu');
-        };
-
+        
         document.addEventListener('click', event => {
             const target = event.target;
             console.log(target);
             const target1 = target.closest('.menu');
             const target2 = target.closest('.close-btn');
             const target3 = target.closest('.active-menu');
-            const target4 = target.closest('menu>ul');
-            if (target1 || target2 || target4) {
-                handlerMenu();
-            } else if (!target3) {
+            if (target1) {
+                menu.classList.add('active-menu');
+            } else if (!target3||target2||target.tagName === 'A') {
                 menu.classList.remove('active-menu');
             }
 
